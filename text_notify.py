fe69cc246aa4404e306eab43cedb16b3aa3f3456
Erik Rhodes <rhodesej@gmail.com>
2014-03-27 00:58:41 -0700

    Improved text_notify
    -Defined as function that imports JSON files with private info
    -Tested and verified

--- text_notify.py
+++ text_notify.py
@@ -1,31 +1,24 @@
 #!/usr/bin/env python
 
 import smtplib
+import json
+from pprint import pprint
+
 # Send a text message notifying them of a new song by artist (based on their choices)
-# Has not been tested
-# Needs to pass person, artist, and song info as args
+# This opens json file containing needed values
 
-import privates
-     
-#Currently the providers aren't being used
-service_providers =
-         {
-             "Rogers" :   "pcs.rogers.com",
-             "Sprint" :   "messaging.sprintpcs.com",
-             "tMobile":   "tmomail.net",
-             "Telus"  :   "msg.telus.com",
-             "Verizon":   "vtext.com",
-             "ATT"    :   "mms.att.net"
-         }
+def text_notify(artist, song_id):
+    json_data=open('privates.json')
+    data = json.load(json_data)
+    json_data.close()
+    message = "New song %s by %s was released today" % (song_id, artist)
+    msg = """From: %s
+    To: %s
+    Subject: New song by %s
+    %s""" % (data['credentials']['username'], data['phonebook']['Erik Rhodes'], artist, message)
 
-message = "New song %s by %s was released today" % (song_id, artist)
-msg = """From: %s
-To: %s
-Subject: New song by %s
-%s""" % (username, phonebook[person], artist, message)
-
-server = smtplib.SMTP('smtp.gmail.com',587)
-server.starttls()
-server.login(username,password)
-server.sendmail(username, phonebook[person], msg)
-server.quit()
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login(data['credentials']['username'],data['credentials']['password'])
+    server.sendmail(data['credentials']['username'], data['phonebook']['Erik Rhodes'], msg)
+    server.quit()
