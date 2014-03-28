b5683657c69770ae40508d8327108d29562ee5bb
Erik Rhodes <rhodesej@gmail.com>
2014-03-28 01:44:47 -0700

    Updated text_notify with new features
    - Message length shortened
    - try/except implemented

--- text_notify.py
+++ text_notify.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+!/usr/bin/env python
 
 import smtplib
 import json
@@ -6,19 +6,27 @@ from pprint import pprint
 
 # Send a text message notifying them of a new song by artist (based on their choices)
 # This opens json file containing needed values
+# TODO: Investigate IMAP 
 
 def text_notify(artist, song_id):
     json_data=open('privates.json')
     data = json.load(json_data)
     json_data.close()
-    message = "New song %s by %s was released today" % (song_id, artist)
+    message = "New song '%s' by '%s' was uploaded today" % (song_id, artist)
     msg = """From: %s
     To: %s
     Subject: New song by %s
     %s""" % (data['credentials']['username'], data['phonebook']['Erik Rhodes'], artist, message)
 
     server = smtplib.SMTP('smtp.gmail.com',587)
-    server.starttls()
-    server.login(data['credentials']['username'],data['credentials']['password'])
-    server.sendmail(data['credentials']['username'], data['phonebook']['Erik Rhodes'], msg)
-    server.quit()
+    #Problem with texts triggering antivirus scan: 'X-Antivirus: avast!... '
+    #Could strip and reforward?
+     
+    try:
+        server.starttls()
+        server.login(data['credentials']['username'],data['credentials']['password'])
+        server.sendmail(data['credentials']['username'], data['phonebook']['Erik Rhodes'], message)
+    except:
+        print "Could not send email"
+    finally:
+        server.quit()
