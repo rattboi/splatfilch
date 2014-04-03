ab3bc4f241458db5033740737e41fb3ae229c55b
rhodeser <rhodesej@gmail.com>
2014-04-03 16:09:00 -0700

    Included channel in the list of URL and title
    -Output file 'URLs.txt' includes the channel as well
    -Improved instructions and comments for gdata library download and file retrieval

--- src/recent_urls.py
+++ src/recent_urls.py
@@ -3,9 +3,13 @@ import gdata.youtube.service
 import json
               
 # To run, install the gdata package
-# http://developers.google.com/youtube/1.0/developers_guide_python#RetrievingVideos
+# Go to https://code.google.com/p/gdata-python-client/downloads/list and download the gdata library
+# Unzip and run 'setup.py install' in the gdata directory
+# Code used here is referenced/taken from http://developers.google.com/youtube/1.0/developers_guide_python#RetrievingVideos
 
-# This program gets the user (channel) and saves the title and URL. 
+# TODO: Installation automation in script 
+# This program gets users (channels) from 'channels.txt', finds the recently uploaded songs, and saves the
+# channel, title, and URL to another file, 'URLs.txt'. 
 def main():
     # open file containing selected channels to search through
     f = open('channels.txt', 'r')
@@ -28,10 +32,12 @@ def SearchAndPrint(user):
     query.orderby = 'viewCount'
     query.racy = 'include'
     feed = yt_service.YouTubeQuery(query)
+    # print to console to verify - not needed in the future
     PrintVideoFeed(feed)
     for entry in feed.entry:
-        SaveURL(entry)
+        SaveURL(entry, user)
  
+    
 def PrintEntryDetails(entry):
     print 'Video title: %s' % entry.media.title.text
     print 'Video watch page: %s' % entry.media.player.url
@@ -40,24 +46,25 @@ def PrintVideoFeed(feed):
     for entry in feed.entry:
         PrintEntryDetails(entry)
 
-def SaveURL(entry):
+def SaveURL(entry, user):
+    '''Saves information to a file with the format  Channel,URL,title\n
+    No spaces are after the commas.'''
     f = open("URLs.txt", "a")
-    song = [entry.media.player.url, entry.media.title.text]
     url = entry.media.player.url
     title = entry.media.title.text
     # do partition when '&' is spotted
     # should make sure '&' doesn't appear normally
     url = url.partition('&')[0]
-    f.write(url + ',' + title + '\n')
+    f.write(user + ',' + url + ',' + title + '\n')
     f.close
 	 
 def read_URLS():
-    '''Sample function for how to read files of channel and URL'''
-    
+    '''Sample function for how to read files with song information'''
     f = open('URLs.txt', 'r')
     lines = f.readlines()
     f.close()
     # To split the entry, partition based on the ','
+    # Note there are now 3 fields -- Channel, URL, title
     lines[0].partition(',')[0]
     # To get rid of '\n', use .rstrip()
     # Do what you want after this ...    
