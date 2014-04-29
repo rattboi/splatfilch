0c06d860b706227267c4f06447af6b8d85d641d8
Eric Krause <krause.ericm@gmail.com>
2014-04-28 17:00:25 -0700

    commiting 3.0 demo file.  not functional yet

--- src/YTChannel.py
+++ src/YTChannel.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
-
 class YTChannel(object):
+    
+    def __init__(self, user_name, id3_regex = "(.+?)( - )(.*)"):
+        self.user_name = user_name
+        self.id3_regex = id3_regex
+        
+    
     user_name = None
     id3_regex = None
+    display_name = None
+
