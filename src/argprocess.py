36b4cbaa6dae8b032c484ae0702f631ff60f869f
Eric Krause <krause.ericm@gmail.com>
2014-05-01 21:07:52 -0700

    run mode is now required
     - splatfilch may now be run with the options 'update' or 'source'
         - update is to perform an update, downloading files
         - source will have additional options such as add, remove, list,
           for management of channels
    - cleaned up the help printout

--- src/argprocess.py
+++ src/argprocess.py
@@ -6,8 +6,20 @@ def get_args():
     # create the top-level parser
     parser = argparse.ArgumentParser(prog='splatfilch', 
         description='splatfilch can has options')
-    parser.add_argument('--verbosity', '-v', action='count', default=0,
-        help="controls how much info is logged [supported: -v, -vv]")
-    parser.add_argument('--stderr', '-s', action='store_true',
-        help="print to stderr instead of logging to file")
+    parser.add_argument('-e', '--exit', action='store_true', help="don't show this message and exit")
+    parser.add_argument('-v', '--verbosity', action='count', default=0,
+        help="increase output verbosity")
+    parser.add_argument('-s', '--stderr', action='store_true',
+        help="print to stderr instead of logging to a file")
+
+    sp = parser.add_subparsers()
+    sp_update = sp.add_parser('update', help='Download new content since last run')
+    sp_source = sp.add_parser('source', help='Manage {list,add,remove} %(prog)s sources')
+
+
+    
     return parser.parse_args()
+
+if __name__ == "__main__":
+    args = get_args()
+    print args.verbosity
