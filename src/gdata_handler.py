a16bc7d9c0d0a82adff8502eb9ab6cbedfe1010b
Eric Krause <krause.ericm@gmail.com>
2014-05-04 22:08:35 -0700

    functions for channel and video searches added
     - basic demo at end, shows uploads a week old or less from monstercat

--- src/gdata_handler.py
+++ src/gdata_handler.py
@@ -2,51 +2,72 @@
 
 from apiclient.discovery import build
 from apiclient.errors import HttpError
-from oauth2client.tools import argparser
 from collections import namedtuple
-import textwrap
+from datetime import datetime, timedelta
+from rfc3339 import rfc3339
 
-channel = namedtuple('channel', 'uuid name description')
-video = namedtuple('video', 'url title')
+channel_t = namedtuple('channel', 'id name description')
+video_t = namedtuple('video', 'id title')
 
-class gdata_handler(object):
-    DEVELOPER_KEY = "AIzaSyDkbupZc60XCCfJmXH39yxyUfOSaQQ03dQ"
-    YOUTUBE_API_SERVICE_NAME = "youtube"
-    YOUTUBE_API_VERSION = "v3"
-    youtube = None
+DEVELOPER_KEY = "AIzaSyDkbupZc60XCCfJmXH39yxyUfOSaQQ03dQ"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+YOUTUBE = None
 
-    def __init__(self):
-        self.youtube = build(
-            self.YOUTUBE_API_SERVICE_NAME,
-            self.YOUTUBE_API_VERSION,
-            developerKey=self.DEVELOPER_KEY)
+def youtube():
+    global YOUTUBE
+    if YOUTUBE == None:
+        YOUTUBE = build(
+            YOUTUBE_API_SERVICE_NAME,
+            YOUTUBE_API_VERSION,
+            developerKey=DEVELOPER_KEY)
+    return YOUTUBE
 
-    def find_channel(self, channel_name):
-        results = []
-        response = self.youtube.search().list(
-            type='channel',
-            q=channel_name,
-            part='id,snippet',
-            maxResults=5,
-            ).execute()
+def find_channels(channel_name, max_results):
+    results = []
+    response = youtube().search().list(
+        type='channel',
+        q=channel_name,
+        part='id,snippet',
+        maxResults=max_results,
+        ).execute()
 
-        for search_result in response.get("items", []):
-            if search_result['snippet']['description'] != "":
-                results.append(
-                    channel(name=search_result['snippet']['title'],
-                        uuid=search_result['snippet']['channelId'],
-                        description=search_result['snippet']['description'])
+    for search_result in response.get("items", []):
+        if search_result['snippet']['description'] != "":
+            results.append(
+                channel_t(
+                    name=search_result['snippet']['title'],
+                    id=search_result['snippet']['channelId'],
+                    description=search_result['snippet']['description']
+                )
             )
-        return results
+    return results
 
+
+def find_videos(channel_id, datetime_since, max_results):
+    results = []
+    response = youtube().search().list(
+        type='video',
+        channelId=channel_id,
+        publishedAfter=rfc3339(datetime_since),
+        part='id,snippet',
+        maxResults=max_results,
+        ).execute()
+
+    for search_result in response.get("items", []):
+        if search_result['snippet']['description'] != "":
+            results.append(
+                video_t(
+                    title=search_result['snippet']['title'],
+                    id=search_result['id']['videoId']
+                )
+            )
+    return results
 
 if __name__ == "__main__":
-    try:
-        h = gdata_handler()
-        r = h.find_channel("Monstercat")
-
-        for i in range(len(r)):
-            print "%d - %s\n    %s\n" % (i+1, r[i].name, r[i].description)
-
-    except HttpError, e:
-        print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+    Monstercat = "UCJ6td3C9QlPO9O_J5dF4ZzA"
+    now = datetime.now()
+    last_week = now - timedelta(days=7)
+    
+    for i in find_videos(Monstercat, last_week, 50):
+        print i.id, i.title
