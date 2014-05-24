bf3d530dd939e37815d52f35147fb499b8b1f230
Eric Krause <krause.ericm@gmail.com>
2014-05-24 12:40:07 -0700

    title_parse now handles a wide variety of inputs
    this commit closes issue #48
    ----------------------------
    
    all unit tests in file are passing.  some random testing will be needed
    to identify patterns of naming not yet anticipated or seen, however the
    rules in this routine should be working a very large majority of the
    time.
    
    on failure, this routine should return ('',input_given), however all
    cases of dirty input have not been tested to ensure it always fails
    gracefully

--- src/regex-examples.py
+++ src/regex-examples.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# pylint: disable=anomalous-backslash-in-string,line-too-long
 import re
 from collections import namedtuple
 
@@ -25,22 +27,82 @@ def title_parse(videoname):
 
     >>> title_parse("Artist & Artist - Title")
     ('Artist & Artist', 'Title')
+
+    >>> title_parse("Savant - 8-Bit Lightsaber (Original Mix) [SectionZ Records]")
+    ('Savant', '8-Bit Lightsaber (Original Mix)')
+
+    >>> title_parse("[Dubstep] - Au5 - Blossom [Monstercat EP Release]")
+    ('Au5', 'Blossom')
+
+    >>> title_parse("*Drumstep* Spag Heddy & Panda Eyes - Nafta")
+    ('Spag Heddy & Panda Eyes', 'Nafta')
+
+    >>> title_parse("Electro House: Zentra - Conditions")
+    ('Zentra', 'Conditions')
+
+    >>> title_parse("SOULERO - Burning Down (Ft. Meron Ryan) - Kaster Mix")
+    ('SOULERO', 'Burning Down (Ft. Meron Ryan) - Kaster Mix')
+
+    >>> title_parse(u"►SubSIL3NT Podcast 081 [Electro/House]")
+    ('', 'SubSIL3NT Podcast 081 [Electro/House]')
+
+    >>> title_parse("HD Dubstep | Teminite - Shockwave [Free Download]")
+    ('Teminite', 'Shockwave')
+
+    >>> title_parse("[Chillstep]: Twofold - Skyfire ( Avien Remix )")
+    ('Twofold', 'Skyfire ( Avien Remix )')
+
+    >>> title_parse("Peking Duk -- High (Yahtzel Remix)")
+    ('Peking Duk', 'High (Yahtzel Remix)')
 """
-    # try the simple case first:
+    # remove unicode characters, if present
+    videoname = videoname.encode('ascii', 'ignore')
+
+    # before counting dashes, fix a rare (stupid) case where uploaders use
+    # 2 dashes instead of 1
+    if '--' in videoname:
+        videoname = re.sub('--', '-', videoname)
+
+    # number of dashes can help to determine format
+    dashes = re.findall(" - ", videoname)
+
+    # it probably isn't a song if there is no dash...
+    if len(dashes) == 0:
+        return ('', videoname)
+
+    # a rare case where '*text*' appears at the front of the title
+    if len(re.findall(r'\*', videoname)) == 2:
+        videoname = re.sub(r'\*(.+?)\* ', '', videoname)
+
+    # a rare case where 'text: ' appears at the front of the title
+    if ': ' in videoname:
+        videoname = re.sub(r'^(.+?): ', '', videoname)
+
+    # a rare case where 'text | ' appears at the front of the title
+    if ' | ' in videoname:
+        videoname = re.sub(r'^(.+?) \| ', '', videoname)
+
+    # commonly, this means '[text] - artist - title ... '
+    # in this case, the leading '[text] -' can be deleted, then
+    # parsing can continue using single-dash algorithm
+    if len(dashes) == 2:
+        videoname = re.sub(r'^\[(.+?)\] - ', '', videoname)
+
+    # single-dash algorithm: split into two fields at the dash,
+    # then remove '[text]' from each, if present
     attempt = re.search(common.re, videoname)
     artist = attempt.group(common.artist)
     title = attempt.group(common.title)
 
-    # remove square bracketed fields from artist, if present
-    if ('[' in artist) or ('[' in artist):
+    # remove square bracketed fields preceeding artist, if present
+    if ']' in artist:
         artist = re.sub('\[(.+?)\] ', '', artist)
 
     # similarly for these fields following the title
-    if ('[' in title) or ('[' in title):
+    if '[' in title:
         title = re.sub(' \[(.+?)\]', '', title)
 
     return (artist, title)
-
 
 if __name__ == "__main__":
     import doctest
